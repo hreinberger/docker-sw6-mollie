37b9686eb705288361958447a622fe801b8dfce4
Christian Dangl <cd@dasistweb.de>
2024-02-12 12:56:15 +0100

    add circleci

---
+++ .circleci/trigger.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# export to temporary file to debug values
+cat <<EOT >> body.json
+{
+  "parameters":
+      {´
+      }
+}
+EOT
+
+curl -X POST -d @body.json -H "Content-Type: application/json" -H "Circle-Token: $1" https://circleci.com/api/v2/project/github/boxblinkracer/docker-sw6-mollie/pipeline
