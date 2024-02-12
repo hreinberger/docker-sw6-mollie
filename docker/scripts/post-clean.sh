988b71bc0deca0d7030d0088c684355f27c9e3b6
Christian Dangl <cd@dasistweb.de>
2024-02-12 09:34:02 +0100

    test

--- docker/scripts/post-clean.sh
+++ docker/scripts/post-clean.sh
@@ -1,11 +1,11 @@
 
 # we dont need the basecom plugin anymore after fixtures exist
 cd /var/www/html && php bin/console --no-debug plugin:uninstall BasecomFixturePlugin
-rm -rf /var/www/html/custom/plugins/FixturesPlugin
 
 # remove our temp folders
 sudo rm -rf /mollie
 sudo rm -rf /scripts
 
 # and finally clear the cache again
+sudo rm -rf /var/www/html/var/cache
 cd /var/www/html && php bin/console --no-debug cache:clear
