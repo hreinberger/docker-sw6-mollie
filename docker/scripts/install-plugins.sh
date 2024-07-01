57c443dcee1b16c27323ded42dcb718fbcfcb887
Christian <boxblinkracer@gmx.net>
2024-07-01 09:02:02 +0200

    fix fixture plugin

--- docker/scripts/install-plugins.sh
+++ docker/scripts/install-plugins.sh
@@ -1,9 +1,7 @@
 
 cd /var/www/html && composer require store.shopware.com/molliepayments
 
-git clone -b main https://github.com/basecom/FixturesPlugin /var/www/html/custom/plugins/FixturesPlugin
-## TODO: remove the checkout, we need it currently for 6.6.2.0
-cd /var/www/html/custom/plugins/FixturesPlugin && git checkout feature/never-vendor-again
+cd /var/www/html && composer require basecom/sw6-fixtures-plugin:3.0.0
 
 cd /var/www/html && php bin/console --no-debug cache:clear
 cd /var/www/html && php bin/console --no-debug plugin:list
