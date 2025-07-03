e780996111bd54e5653721cc4774290dd25d19fc
Hannes Reinberger <40197225+hreinberger@users.noreply.github.com>
2025-07-03 10:01:51 +0200

    adapt to Shopware 6.7 and simplify

--- .circleci/trigger.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# export to temporary file to debug values
-cat <<EOT >> body.json
-{
-}
-EOT
-
-curl -X POST -d @body.json -H "Content-Type: application/json" -H "Circle-Token: $1" https://circleci.com/api/v2/project/github/boxblinkracer/docker-sw6-mollie/pipeline
